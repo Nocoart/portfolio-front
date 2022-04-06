7c3b08e496710a11b1e00242f8a3b05d5dacbc8d
Nicolas Guerineau <nicolasguerineau@MacBook-Air.com>
2022-04-06 16:04:10 +0200

    sandbow update

--- src/Components/Sandbox/Sandbox.js
+++ src/Components/Sandbox/Sandbox.js
@@ -1,7 +1,133 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import { useInView, InView } from "react-intersection-observer";
+import { gsap, TweenMax, Power3 } from "gsap";
+import "./Sandbox.scss";
 
 const Sandbox = () => {
-  return <div></div>;
+  const header = useRef(null);
+  const refleft = useRef(null);
+
+  const { ref: card1, inView: cardInView1, entry1 } = useInView({ threshold: 1 });
+  const { ref: card2, inView: cardInView2, entry2 } = useInView({ threshold: 1 });
+
+  useEffect(() => {
+    gsap.to(header.current, { color: "red", duration: 2 });
+  }, []);
+
+  return (
+    <div>
+      <h1 ref={header} style={{ fontSize: "40px", fontFamily: "Game Of Squids" }}>
+        TITLE
+      </h1>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte" ref={card1}>
+        {`carte observed ${cardInView1 ? "yes" : "no"}`}
+      </div>
+      <div className="carte" ref={card2}>
+        {`carte observed ${cardInView2 ? "yes" : "no"}`}
+      </div>
+
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+      <div className="carte">carte</div>
+
+      {/* <div className="carte" ref={refleft}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refright}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refleft}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refright}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refleft}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refright}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refleft}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refright}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refleft}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refright}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refleft}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refright}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refleft}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refright}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refleft}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refright}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refleft}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refright}>
+        {`carte ${inView}`}
+      </div>
+      <div className="carte" ref={refleft}>
+        {`carte ${inView}`}
+      </div> */}
+    </div>
+  );
 };
 
 export default Sandbox;
--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../../Components/Header/Header";
+import Sandbox from "../../Components/Sandbox/Sandbox";
 
 //styles
 import "./Home.scss";
@@ -8,11 +9,12 @@ import "./Home.scss";
 const Home = () => {
   return (
     <div className="container home">
-      <Header />
+      <Sandbox />
+      {/* <Header />
       <h1>
         Nicolas <span className="pink">G</span>uerineau
       </h1>
-      <Link to="/about">{`{`}</Link>
+      <Link to="/about">{`{`}</Link> */}
     </div>
   );
 };
